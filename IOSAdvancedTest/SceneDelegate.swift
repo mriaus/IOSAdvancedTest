be35b4325041929fea603386d475fb2cb646ab65
mriaus <55255215+mriaus@users.noreply.github.com>
2023-10-23 11:33:02 +0200

    Added the SplashViewmodel file and linked the Splash storyboard with the SplashViewController and added the changes in the SceneDelegate to remove the default flow

--- IOSAdvancedTest/SceneDelegate.swift
+++ IOSAdvancedTest/SceneDelegate.swift
@@ -16,7 +16,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let storyboard = UIStoryboard(name: "Splash", bundle: .main)
+        
+        let rootViewController = storyboard.instantiateViewController(withIdentifier: "SplashViewController") as? SplashViewController
+//        TODO: Add the Viewmodel when
+
+        let window = UIWindow(windowScene: scene)
+        window.rootViewController = UINavigationController(rootViewController: rootViewController ?? UIViewController())
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
